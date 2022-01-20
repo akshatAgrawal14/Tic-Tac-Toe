9b869cbfa4f631f63473eb611bd7ac3ad6270998
akshatAgrawal14 <59191060+akshatAgrawal14@users.noreply.github.com>
2022-01-20 21:04:22 +0530

    Update tic tac toe program 1.py

--- tic tac toe program 1.py
+++ tic tac toe program 1.py
@@ -1,13 +1,5 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-'''
-def draw_board(board):
-    print(board[7] + " |" + board[8] + " |" + board[9])
-    print("--+--+--")
-    print(board[4] + " |" + board[5] + " |" + board[6])
-    print("--+--+--")
-    print(board[1] + " |" + board[2] + " |" + board[3])
-'''
 
 def draw_board(board):
     print(board[1] + " |" + board[2] + " |" + board[3])
